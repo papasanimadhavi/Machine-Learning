1682b90d11204565f18bb3ca9e7b51e97a6ef6ce
papasanimadhavi <95456798+papasanimadhavi@users.noreply.github.com>
2023-08-16 10:02:11 +0530

    Update find_s.py

--- find_s.py
+++ find_s.py
@@ -1,34 +1,24 @@
+#1 find-s 
 import numpy as np
 import pandas as pd
+df=pd.read_csv(r'C:\Users\hp\OneDrive\Desktop\ENJOYSPORT.csv')
+print(df)
 
-k=pd.DataFrame(pd.read_excel('/content/temperature.xlsx'))
-print(k)
-
-k=np.array(k)
-print(k)
-
-t=k[:,-1]
-print(t)
-print(k[0][0])
-
-h=[0,0,0,0,0,0]
-n=len(h)
-for i in range(len(h)):
-  for j in range(len(t)):
-    if t[j]=='yes':
-        h[i]=k[0][i]
-        break
-    
-print(h)
-
-
-for i in range(0,len(k)):
-    if k[i][len(h)]=='yes':
-      for j in range(0,len(h)):
-          if h[j]!=k[i][j]:
-              h[j]='q'
-          else:
-              h[j]=k[i][j]
+h=["","","","","",""]
+for index,row in df.iterrows():
+    ind=0
+    if row['EnjoySport']==1:
+        for i in range(len(row)-1):
+            if h[ind]=='?':
+                ind+=1
+                continue
+            elif h[ind]==row[i]:
+                ind+=1
+                continue
+            elif h[ind]=="":
+                h[ind]=row[i]
+            elif h[ind]!=row[i]:
+                h[ind]='?'
+            ind+=1
             
 print(h)
-
